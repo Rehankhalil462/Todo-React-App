f71d57d0f0becb1c84d51497668783c3d4ab6798
Rehan Khalil <rehankhalil462@gmail.com>
2021-02-05 01:16:40 +0500

    trying to make pwa

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@ const appVersion = 'v1.00';
 // Files to Cache
 const files = [
   '/',
-  'index.html',
+  '/index.html',
   'manifest.json',
   '64x64.png',
   '192x192.png',
@@ -55,10 +55,10 @@ self.addEventListener('activate', (event) => {
 
 //Fetch
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match('offline.html'));
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
     })
   );
 });
